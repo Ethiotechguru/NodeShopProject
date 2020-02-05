84e16938b63e361663240993b566e18b320f6676
Samuel Belete <samuelbelete@Samuels-MacBook-Pro.local>
2020-02-04 22:50:32 -0800

    controller for 404

--- router/404.js
+++ router/404.js
@@ -1,9 +1,7 @@
 const path  = require('path')
 const express = require('express');
 const router = express.Router();
-
-router.use('/', (req, res)=>{
-    res.render('404.ejs', {pageTitle:"Page Not Found", path:'/'})
-})
+const errorController = require('../controllers/404')
+router.use('/', errorController.get404)
 
 module.exports = router;
